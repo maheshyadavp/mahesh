1d684c006241650812159898803677fcf82765cf
maheshyadavp <maheshdv67@gmail.com>
2022-08-04 18:48:00 +0530

    first commit

---
+++ src/main/java/com/mahesh/pranesh/demo.java
@@ -0,0 +1,14 @@
+package com.mahesh.pranesh;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int b=20;
+		int c=30;
+		int d=40;
+		System.out.println(d+b+c);
+
+	}
+
+}
